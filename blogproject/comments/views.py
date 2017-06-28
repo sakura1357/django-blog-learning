9931e7866e57bb3d06f6d3d68b6bf89f608e8e5f
sakura1357 <sakura1357@126.com>
2017-06-28 12:23:07 +0800

    push

--- blogproject/comments/views.py
+++ blogproject/comments/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Comment
+from .forms import CommentForm
+
+from blog.models import Post
 
 # Create your views here.
+def post_comment(request, post_pk):
+
+    post = get_object_or_404(Post, pk=post_pk)
+
