567cf54af33b524fc6e9a7d490c7ad61fde8d0be
sakura1357 <sakura1357@126.com>
2017-06-29 10:36:55 +0800

    bind url

---
+++ blogproject/comments/urls.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+from django.conf.urls import url
+from . import views
+
+
+'''
+@author: siar
+@contact: xzt1357@gmail.com
+@file: urls.py
+@time: 2017/6/28 16:14
+@desc:
+'''
+
+app_name = 'comments'
+urlpattern = [
+    url(r'^comment/post/(?P<post_pk>[0-9]+)/$', views.post_comment, name='post_comment')
+]
