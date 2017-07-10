b32ea6e3d1bb3bbaa686e34d6f893932f413320c
sakura1357 <sakura1357@126.com>
2017-07-10 13:49:58 +0800

    modify to product environment.

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&wy$1)-tm@5cmhldvo+4e8l5jle0gmzdjjl7_xt9vzanokb3x1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.siar.me']
 
 
 # Application definition
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 HAYSTACK_CONNECTIONS = {
     'default': {
